3e25e07d462634004aac4c1ea6e39d872e4085db
EHOT13 <vanchez@mail.ua>
2017-03-10 16:53:59 +0300

    Task 1 completed

--- LR2/Task#1/1/1/_1.py
+++ LR2/Task#1/1/1/_1.py
@@ -1,19 +1,33 @@
-my_file = open('text.txt','r')  #Open file in readonly mode
+my_file = open('text.txt','r')       #Open file in readonly mode
 
-#print(my_file.read())   #Printing all symbols
+#print(my_file.read())               #Printing all symbols
 my_string = my_file.read()
-my_string = my_string.lower() #lower reg
-setted_string = set(my_string)
-#TODO: ignore/delete symbols
-
-
-"""
+my_string = my_string.lower()        #lower reg
+noSymb = []                          #array for letters without symbols(#!^&*$...)
 for i in range(len(my_string)):
+    if my_string[i].isalpha():
+        noSymb.append( my_string[i] )
+
+#------------------------------------
+dict = {}                            #Frequensy of letter
+for i in range(len(noSymb)):
     counter = 0
-    temp = my_string[i]
-    for j in range(len(my_string)):
-"""
-
-        
-
+    for j in noSymb:
+        if noSymb[i] == j:
+            counter = counter + 1
+            dict[noSymb[i]] = counter
+ #-----------------------------------
+                                     #Sorting by freq
+#SortedValues = dict.values()
+#SortedValues = list(SortedValues)
+#SortedValues.sort(reverse = True)
+#print(SortedValues)
+#print( sorted(dict.items(), reverse=True))     
+l = lambda x: x[1]
+SortedValues = sorted(dict.items(), key=l, reverse=True) 
+#-----------------------------------
+                                    #Output
+print ('Unsorted: \n',dict,"\n\n",'Sorted: \n',SortedValues)
+#print (SortedValues)
+#TODO: Sort by param
 my_file.close()
